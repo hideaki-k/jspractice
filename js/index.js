9ed3d0294ec9306c00a3875079c2c1d488d33633
hideaki-k <h.kinoshita@kimura-lab.net>
2023-08-19 23:36:31 +0900

    スライドショー表示

--- js/index.js
+++ js/index.js
@@ -1,41 +1,94 @@
-var answer = ["なるほど!","ふむふむ","フーン"];
+// スライダーに表示する画像のパス
+var imgList = [
+	"images/img01.jpg",
+	"images/img02.jpg",
+	"images/img03.jpg",
+	"images/img04.jpg"
+]
 
-// メッセージ送信処理　引数：送信したいメッセージ(文字列)
-function addChatText(val, type){
+// 画像とナビの要素を自動で追加
+for (var i=0; i<imgList.length;i++){
+	// li要素を取得
+	var slide = document.createElement("li");
+	// li要素の中に画像タグを埋め込む
+	slide.innerHTML = "<img src='" + imgList[i] + "'>";
+	// li要素をクラス名「slider-inner」の子要素として追加
+	document.getElementsByClassName("slider-inner")[0].appendChild(slide);
 
-
-  // チャットに追加するHTMLのタグを生成
-    var text = document.createElement('li');
-  // テキストを設定
-    text.innerHTML = val;
-  // 赤文字のスタイルを設定（CSSのclass「message-other」を設定）
-    if(type === "other"){
-        text.classList.add("message-other");
-    }
-  // 追加対象となる要素を取得
-    var chatWindow = document.getElementById("chat-list");
-  // appendChild()メソッドで要素とテキストをHTMLに追加
-    chatWindow.appendChild(text);
+	// li要素を取得
+	var nav = document.createElement("li");
+	// プロパティ「data-nav-index」に数値を割り振る
+	nav.setAttribute("data-nav-index",i);
+	// li要素をクラス名「nav」の子要素として追加
+	document.getElementsByClassName("nav")[0].appendChild(nav);
 }
 
-// 送信ボタンを押した時にメッセージを送信
-document.getElementById("chat-send").addEventListener("click",function(){
-    var inputText = document.getElementById("chat-input");
-  // addChatText関数を入力値とCSSのclass判別文字列を引数として実行
-    addChatText(inputText.value,"you");
-  // 入力欄を空欄にする
-    inputText.value = "";
-});
 
-// 8秒毎にメッセージを送信
-setInterval(function(){
-  // ランダムの整数を設定
-    var index = Math.floor(Math.random()*answer.length);
-  // ランダムメッセージを表示
-    addChatText(answer[index],"other");
-}, 8000);
+// スライドの数を取得(処理のために-1する
+var length = imgList.length - 1;
+// クラス名「imageSlide」に画像の1枚の要素を格納
+	var imageSlide = document.getElementsByClassName("slider-inner")[0].getElementsByTagName("li");
+// クラス名「dotNavigation」にドットナビの1つの要素を格納
+	var dotNavigation = document.getElementsByClassName("nav")[0].getElementsByTagName("li");
+// 「現在○○枚目のスライドを表示している」というインデックス番号を格納する変数
+	var nowIndex = 0;
+// 現在表示されている画像とドットナビにクラス名を付ける
+	imageSlide[nowIndex].classList.add("show");
+	dotNavigation[nowIndex].classList.add("current")
+// スライドがアニメーション中か判断するフラグ
+	var isChanging = false;
+// スライドのsetTimeoutを管理するタイマー
+	var slideTimer;
+// スライド切り替え時に呼び出す関数
+	function sliderSlide(val){
+		if (isChanging === true){
+			return false;
+		}
+		isChanging = true;
+		// 現在表示している画像とナビからクラス名を削除
+		imageSlide[nowIndex].classList.remove("show");
+		dotNavigation[nowIndex].classList.remove("current");
+		nowIndex = val;
+		// 次に表示する画像とナビにクラスを付与
+		imageSlide[nowIndex].classList.add("show");
+		dotNavigation[nowIndex].classList.add("current");
+		//アニメーションが終わるタイミングでisChangingのステータスをfalseに
+		slideTimer = SetTimeout(function(){
+			isChanging = false;
+		}, 600);
+	}
 
-// 初回のみ1秒後にメッセージを自動送信
-setTimeout(function(){
-    addChatText("こんにちは!","other");
-}, 1000);
+
+
+
+
+
+
+
+
+
+
+
+// 左矢印のナビをクリックした時のイベント
+
+
+
+
+
+
+
+// 右矢印のナビをクリックした時のイベント
+
+
+
+
+
+
+
+// ドットナビをクリックした時のイベントを作成
+
+
+
+
+
+
